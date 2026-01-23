e472c1c8cbaebf85be5c193cb4766e276b92fcd0
nakulyadav <nakulyadav1710@gmail.com>
2026-01-23 18:36:22 +0530

    first ccommit

---
+++ main.py
@@ -0,0 +1,61 @@
+# this is my first python code
+# print("I like pizza")
+# print("I really like it")
+
+
+# variable = A contain for a value (string, interger , float, boolean)
+# A variable behaves as if it was the value it contains
+
+
+#string
+first_name = "Nakul"
+food = "Pizza"
+email = "nakulyadav1710@gmail.com"
+
+# print(f"Do you like {food}?")
+# print(f"Is your email is {email}")
+# print(f"Your name is {first_name}")
+
+#Always put f before quotes when using {} in strings.
+
+
+
+#Integer
+
+age = 10
+limit = 3
+num_of_student = 30
+
+# print(f"Your age is {age}")
+# print(f"The number of student is {num_of_student}")
+
+
+
+#floats
+
+price = 10.99
+distance = 5.5
+
+# print(f"The price is : ${price}")
+# print(f"you run about {distance}miles")
+
+
+#Boolean 
+
+is_student = True
+is_online = False
+
+# if is_student :
+#      print("You are a student")
+# else:
+#      print("You are not a student")
+
+
+# if is_online :
+#      print("You are Online")
+# else:
+#      print("You are Offline")
+
+
+
+     
