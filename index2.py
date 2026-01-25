7b17fb075c186c87f34b4d7b590009c80c88472d
nakulyadav <nakulyadav1710@gmail.com>
2026-01-25 20:56:13 +0530

    python-maths

---
+++ index2.py
@@ -0,0 +1,18 @@
+#input : take input from the users
+
+#exercise1 find rectange area
+
+# length = float(input("Length of rectangle: "))
+# width = float(input("Width of rectangle: "))
+
+# area = length * width
+# print(f"The area is : {area}cm^2")
+
+#Shopping cart program
+
+item = input("What item would you like: ")
+price = float(input("What is the price of the item?: "))
+quantity = int(input("How many  items do you like? : "))
+
+total = price * quantity
+print(f"your total is : ${total}")
---
+++ index3.py
@@ -0,0 +1,44 @@
+
+
+x = 3.14754895723987
+# y = 4
+# z = 5
+
+
+
+# result = round(x) #// convet 3.14754895723987 to 3
+# result = abs(y) #negative  to  positive
+# result = pow(2,4) 
+# result = max(x,y,z)
+# result = min(x,y,z)
+
+
+
+import math
+#have to import math
+
+
+
+# print(math.pi)
+# print(math.e)
+# print(math.sqrt(x))
+# print(math.ceil(x)) #4
+# print(math.floor(x)) #3
+
+
+radius = float(input("Enter the radius of the circle:"))
+
+area = math.pi * pow(radius,2)
+
+print(f"The area of the circle is: {round(area)}cm^2")
+
+
+
+
+
+
+
+
+
+
+
