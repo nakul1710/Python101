360b9314b6928d25ec2b948762a207e18258bd27
nakulyadav <nakulyadav1710@gmail.com>
2026-01-26 22:20:42 +0530

    StringIndex

---
+++ index7.py
@@ -0,0 +1,19 @@
+# Indexing = accessing elements of sequence using [] (indexing operator)
+#   [start : end : step]
+
+
+credit_number = "7582-7781-8282-6732"
+
+#print(credit_number[3])
+#print(credit_number[0:4])
+#print(credit_number[5:9])
+# print(credit_number[5:]) #rest of the chracter after index 5
+# print(credit_number[-1]) #print all digit in reverse of the string
+# print(credit_number[::2])
+last_digits = credit_number[-4:]
+print(f"XXXX-XXXX-XXXX-{last_digits}")
+
+
+reverse_digit_number = credit_number[::-1]
+print(f"{reverse_digit_number}")
+
