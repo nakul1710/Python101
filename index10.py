e6b5069649135dac7b384558f9161463d80d1b1c
nakulyadav <nakulyadav1710@gmail.com>
2026-01-27 23:32:27 +0530

    nested-loop

---
+++ index10.py
@@ -0,0 +1,35 @@
+#For Loop
+
+
+# for x in range(1,11):
+#     print(x)
+# print("Happy new year!")
+
+
+#Reversed for loop
+
+# for x in reversed(range(1, 11)):
+#     print(x)
+# print("Happy new year!")
+
+
+# for x in range(10,0,-1):
+#     print(x)
+
+
+#continue statement
+for x in range(1,21):
+    if x == 13:
+        continue
+    else:
+        print(x)
+
+
+#break statement
+for x in range(1,21):
+    if x == 13:
+        break
+    else:
+        print(x)
+    
+
