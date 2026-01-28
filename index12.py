5d694160cd33d2fd8d1fb613644a1ca795e75d6a
nakulyadav <nakulyadav1710@gmail.com>
2026-01-28 20:42:59 +0530

    List,tuple & sets

--- index12.py
+++ index12.py
@@ -8,7 +8,7 @@ for x in range (my_time,0,-1):
     minutes = int(x/60)%60
     hours = int(x/3600)
     print(f"{hours:02}:{minutes:02}:{seconds:02}")
-    time.sleep(0.05)
+    time.sleep(1)
 
 
 print("Time's up!")
---
+++ index13.py
@@ -0,0 +1,48 @@
+# Collection : Single "variable" used to stroe multiple values
+#Lists = [] ordered and change-able .Duplicate ok
+# Sets = {} unordered  and immutable, but add/remove ok. No Duplicates
+# Tuples = () ordered and unchange-able . Duplicates are ok
+
+
+##1 * Lists *
+
+
+#fruits = ["Mango","banana","Coconut","Orange"]
+
+# print(fruits[0])
+# print(fruits[0::3])
+# print(fruits[::-1])
+# print(fruits[::2])
+
+
+#print(dir(fruits))   Bunch of Functions in lists
+#print(help(list))
+ #print(len(fruits))
+
+#fruits[0] = "Pinapple"
+#fruits.append("Pineapple")
+#fruits.remove("Mango")
+#fruits.insert(0,"Pineapple")
+#fruits.sort()
+#fruits.reverse()
+#print(fruits.index("banana"))
+
+# for fruits in fruits:
+#     print(fruits)
+
+#print(fruits)
+
+
+##2 * Sets *
+
+fruits = {"Mango" , "banana" , "Coconut" , "Orange"}
+
+# fruits.add("Pineapple")
+# fruits.remove("Orange")
+# fruits.pop()
+# fruits.clear()
+
+
+
+
+print(fruits)
