873d97e36809a93f6dfbf84975368ee23b9dc5a3
nakulyadav <nakulyadav1710@gmail.com>
2026-02-08 18:06:54 +0530

    Iterables

---
+++ python/index24.py
@@ -0,0 +1,33 @@
+#Iterables = An object/Collection that can return its elements one at a time . 
+#              Allowing it to itreted over in  a loop
+
+
+# numbers = [1,2,3,4,5]
+
+# for number in reversed(numbers):
+#     print(number, end=" ")
+
+
+#set you can't reverse
+# fruits = {"apples" ,"orange", "banana", "coconut"}
+# print(fruits)
+
+# for fruit in reversed(fruits):
+#         print(fruit)
+
+
+#String
+name = "Bro Code"
+
+for chracter in name:
+    print(chracter, end=" ")
+
+
+#Dictionary
+my_dictionry = {"A": 1 ,"B": 2, "C": 3}
+
+for key , value in my_dictionry.items():
+    print(key,value)
+
+
+    
