616180f643c6c3f745e2949c400c61f08d707aba
nakulyadav <nakulyadav1710@gmail.com>
2026-03-13 23:13:49 +0530

    @genrators for numbers and chracters

--- PythonObjOriented/index29.py
+++ PythonObjOriented/index29.py
@@ -1,16 +1,23 @@
-#Genrator = Function that behave like an iterators (it can be used in a for loop)
+# Generator = Function that behaves like an iterator (it can be used in a for loop)
 
-def cout_no(n):
-    numbers = []
+def count_no(n):
     count = 1
     while count <= n:
-        numbers.append(count)
+        yield count
         count += 1
-    return numbers
 
+numbers = int(input("Enter a number: "))
 
-numbers = int(input("Enter a number:"))
-
-for n in cout_no(numbers):
+for n in count_no(numbers):
     print(n)
 
+# For characters
+
+def char_generator(text):
+    for ch in text:
+        yield ch
+
+word = input("Enter a word: ")
+
+for c in char_generator(word):
+    print(c)
