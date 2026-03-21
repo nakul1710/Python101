2fc77262beeb0502b3420972551b858ad82fbf9e
nakulyadav <nakulyadav1710@gmail.com>
2026-03-21 08:38:53 +0530

    sorting-algoriths

---
+++ DSA-py/index5.py
@@ -0,0 +1,20 @@
+#quick sort
+def quickSort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+
+def partition(arr, low, high):
+    pivot = arr[high]   # choose last element as pivot
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
